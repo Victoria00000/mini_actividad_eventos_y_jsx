6b0ceec8407fc3a08de3e905c6825ebcd48d888a
Victoria00000 <vvictoriaandres@gmail.com>
2022-02-02 04:39:45 -0300

    primer commit del ejercicio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
-
+import flor from './img/rose2.jpg';
+import vaquita from "./img/vaquita.jpg";
+import { useRef } from "react";
+// Ejercicios de eventos, casi sin variables a propósito!!!
 function App() {
+  //Hook de referencia, useRef(), suplanta los getElements.
+  const refCaja = useRef();
+  const moneda = 23.19;
+
+  const incrementar = (evt) => {
+    evt.target.innerHTML = Number(evt.target.innerHTML) + 1;
+    evt.target.style.backgroundColor = 'rgb(127, 229, 255)';
+    if (Number(evt.target.innerHTML) >= 10) { evt.target.innerHTML = 1 };
+    if (Number(evt.target.innerHTML) > 5) { evt.target.style.backgroundColor = 'rgb(202, 97, 97)' };
+  };
+  const convertir = () => refCaja.current.innerHTML = Number(refCaja.current.innerHTML) * moneda;
+  const leer = (evt) => refCaja.current.innerHTML = evt.target.value;
+  //const intercambio = (evt) => evt.target.src = vaquita; //se puede
+  //const intercambio = (evt) => evt.target.src = flor ? evt.target.src = vaquita : evt.target.src = flor; //no se puede
+  const intercambio = (evt) => evt.target.src.includes('rose') ? evt.target.src = vaquita : evt.target.src = flor;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h4> Eventos, inicial </h4>
+      <div className='caja' ref={refCaja} onClick={incrementar}> 1 </div>
+      <button className='button' onClick={convertir}> - Convertir - </button>
+      <div><img src={flor} alt='florImg' title='flor' onClick={intercambio} /></div>
+      <input className='campo' type='text' onChange={leer} />
+    </>
   );
 }
 
